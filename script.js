1ace0a630ea865f89acc9db2ed5a5e2c66d58f48
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2024-11-02 14:33:39 +0530

    qr

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-// Get references to HTML elements
-const openCameraButton = document.getElementById('open-camera');
-const video = document.getElementById('video');
-const captureButton = document.getElementById('capture');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+// script.js file
 
-// Open the camera when "Open Camera" button is clicked
-openCameraButton.addEventListener('click', () => {
-    // Access the webcam
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-            video.srcObject = stream;
-            video.style.display = 'block';
-            captureButton.style.display = 'block';
-            canvas.style.display = 'block';
-        })
-        .catch((error) => {
-            console.error("Error accessing the webcam:", error);
-        });
-});
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
-// Capture the image when "Capture Image" button is clicked
-captureButton.addEventListener('click', () => {
-    // Set canvas dimensions to match video dimensions
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    // Draw the current video frame to the canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+domReady(function () {
+
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText, decodeResult);
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
 });
