8244347542b037a1580f71e2b6ad62abe7e0867d
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2024-11-12 09:40:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,6 @@
-// script.js file
-
-function domReady(fn) {
-    if (
-        document.readyState === "complete" ||
-        document.readyState === "interactive"
-    ) {
-        setTimeout(fn, 1000);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
-
-domReady(function () {
-
-    // If found you qr code
-    function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
-    }
-
-    let htmlscanner = new Html5QrcodeScanner(
-        "my-qr-reader",
-        { fps: 10, qrbos: 250 }
-    );
-    htmlscanner.render(onScanSuccess);
+const ele = document.getElementById("btn")
+ele.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ time: "1" }, function (response) {
+        console.log(response);
+    });
 });
-
-// Get the audio element
-const scanSound = document.getElementById("scan-sound");
-
-// Function to handle successful QR scan
-function onScanSuccess(decodedText) {
-    document.getElementById("result-text").innerText = "QR Code Result: " + decodedText;
-
-    // Play sound after scanning
-    scanSound.play();
-
-    // Optionally stop the scanner after a successful scan
-    html5QrcodeScanner.stop().then(() => {
-        console.log("QR Code scanning stopped.");
-    }).catch(err => console.log("Error stopping scanner:", err));
-}
